2da8ace00bf51dfe531f48aa8bdc5a0dcc7d65b7
mohanadft <mohanadfteha@gmail.com>
2025-01-06 12:06:59 +0200

    configure nextjs to host images from uploadThing

--- next.config.js
+++ next.config.js
@@ -12,6 +12,17 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
@@ -10,7 +9,7 @@ export default async function HomePage() {
     <main className="p-4">
       {images.map((image, index) => (
         <div key={image.id + "-" + index} className="flex w-48 flex-col">
-          <Image src={image.url} alt={image.name} />
+          <img src={image.url} alt={image.name} />
           <div>{image.name}</div>
         </div>
       ))}
